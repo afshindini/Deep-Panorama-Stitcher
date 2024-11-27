718d52e97c2307d49058fb088bb20a9a517ccd82
Afshin Dini <afshin_dini@yahoo.com>
2024-11-27 13:51:24 +0200

    Add logging

---
+++ src/panaroma_stitcher/__init__.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:502229f8423f35c1097ae8fc1bcb556e7a519178ba0bf92727d961177287e859
+size 78
---
+++ src/panaroma_stitcher/logging.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:10435bf6cd801c937ca409f59c71e6610e19adcbed22bd0e7aca307bf4e6cb2e
+size 906
