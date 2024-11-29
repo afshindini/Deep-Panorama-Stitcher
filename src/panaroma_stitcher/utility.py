4234d94954c49dd0c7b9051afd58ebf28c65c1df
Afshin Dini <afshin_dini@yahoo.com>
2024-11-29 13:25:09 +0200

    Add multiple files extension for images + modify teh save_result

--- src/panaroma_stitcher/utility.py
+++ src/panaroma_stitcher/utility.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7b1d85fea0823aa307aad4762016ac23b0e3cbb4e6c3d0136d4a7ba310df938
-size 2834
+oid sha256:daf3d0b122cf87bcea8714b6d114a39e7022bc9998529ab5d95266672a2e002b
+size 2805
