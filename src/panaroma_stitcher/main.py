bec0bddc0d7466376a9ad514ce7d261fa67e06e9
Afshin Dini <afshin_dini@yahoo.com>
2024-11-30 22:51:46 +0200

    Add keypoint stitcher to main CLI

--- src/panaroma_stitcher/main.py
+++ src/panaroma_stitcher/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c00833a1274a9899ed0e3c56f49cae68e09c62a38cb0c9d6d8d7ff424374779
-size 3661
+oid sha256:88b52b5e9de723dd7e23231b4a0489a1c3fc4a0abcdd9ae32e4e4efd8ca113cc
+size 4754
