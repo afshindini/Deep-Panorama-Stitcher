b0e2acca9091441c98d97ab7e2f314d6a7f82cd8
Afshin Dini <afshin_dini@yahoo.com>
2024-11-30 23:39:42 +0200

    Add key point stitcher method

---
+++ src/panaroma_stitcher/keypoint_stitcher.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dc243c9e266e1d3bb265af4f2458c04cfe2d406042508993f8b471a9cc4fbc8c
+size 5492
--- src/panaroma_stitcher/main.py
+++ src/panaroma_stitcher/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88b52b5e9de723dd7e23231b4a0489a1c3fc4a0abcdd9ae32e4e4efd8ca113cc
-size 4754
+oid sha256:e96b5fc238c12dce9eb34b45f7d2b458d40415044260528221b989d5952007fa
+size 4732
