7b7c1cd9482f830db4899119699a91eb70dc6979
Afshin Dini <afshin_dini@yahoo.com>
2024-12-02 00:10:45 +0200

    Update default value of keypoint stitcher

--- src/panaroma_stitcher/main.py
+++ src/panaroma_stitcher/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4495f8a7e721f2534fa8d74e5bd724dc0d69bc9e371014e2887adf62e7b38875
-size 6578
+oid sha256:dea80839f1607d0f459dd2f3e250361fffaad2ecda8a7b8c993a42f70af8e56c
+size 6579
