d87b5ae5c0ff28e5d7b13b0e9b7c76f9f5183928
Afshin Dini <afshin_dini@yahoo.com>
2024-12-05 01:38:23 +0200

    add init

--- src/panaroma_stitcher/__init__.py
+++ src/panaroma_stitcher/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50240e8a5fb7145ddbb16dbe30452d959dac715e0a0414bf366f3f56a602d07c
-size 73
+oid sha256:2e96301e7c7991d30e0d3a80d7924f9cea44fc4fee76727eec15bb5e7c85cc92
+size 406
