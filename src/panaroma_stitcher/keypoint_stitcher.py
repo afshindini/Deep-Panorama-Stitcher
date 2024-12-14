59e7b7f72e4f1025bc56cda62698304f36502a32
Afshin Dini <afshin_dini@yahoo.com>
2024-12-15 00:51:46 +0200

    Add framer to keypoint stitcher & remove some codes

--- src/panaroma_stitcher/keypoint_stitcher.py
+++ src/panaroma_stitcher/keypoint_stitcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc243c9e266e1d3bb265af4f2458c04cfe2d406042508993f8b471a9cc4fbc8c
-size 5492
+oid sha256:37bc9a35c81495733cb37dcdd23c953f3006869d683077a5693b9e35d72dc041
+size 5407
