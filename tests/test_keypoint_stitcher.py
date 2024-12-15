ba9e3777f56082b22a41741ada9c900f4ef1a5e3
Afshin Dini <afshin_dini@yahoo.com>
2024-12-16 00:19:14 +0200

    Update unit test files for adding framer

--- tests/test_keypoint_stitcher.py
+++ tests/test_keypoint_stitcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49fcadbdb749608821fdbd3982c296ff158899d0dd9f10274bbd0aee723f7a8b
-size 1664
+oid sha256:d009e0d323f0d1749e1bf28127ad762f8766966671b1640e690bfc4bbe26891c
+size 1670
--- tests/test_opencv_simple.py
+++ tests/test_opencv_simple.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07a9b2d7938dbd838534941c2c91eff8e88988c6dac9bd995851fc66aadf187d
-size 861
+oid sha256:ea0d7c19268717992cb3b3c09984fb107394310e258f7dd5ceb37160430db9a0
+size 867
